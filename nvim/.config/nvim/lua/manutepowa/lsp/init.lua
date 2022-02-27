4d529b19df31ad28bcad3619fc9c3825bc38fcb9
Manuel <manutepowa@gmail.com>
2022-02-27 23:00:03 +0100

    ..

--- nvim/.config/nvim/lua/manutepowa/lsp/init.lua
+++ nvim/.config/nvim/lua/manutepowa/lsp/init.lua
@@ -3,7 +3,7 @@ if not status_ok then
 	return
 end
 
-require("manutepowa.lsp.lsp_signature")
-require("manutepowa.lsp.lsp_installer")
+require "manutepowa.lsp.lsp_signature"
+require "manutepowa.lsp.lsp_installer"
 require("manutepowa.lsp.handlers").setup()
-require("manutepowa.lsp.null-ls")
+require "manutepowa.lsp.null-ls"
