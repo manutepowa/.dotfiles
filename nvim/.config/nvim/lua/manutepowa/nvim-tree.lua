2bae0b688af55902ee988271230ad81099b85121
Manuel <manutepowa@gmail.com>
2022-03-28 18:54:10 +0200

    Change dap port for 9003 because ddev use it after v1.19

--- nvim/.config/nvim/lua/manutepowa/nvim-tree.lua
+++ nvim/.config/nvim/lua/manutepowa/nvim-tree.lua
@@ -88,7 +88,6 @@ nvimtree.setup {
   hijack_netrw = true,
   open_on_setup = false,
   ignore_ft_on_setup = {},
-  auto_close = true,
   open_on_tab = false,
   hijack_cursor = false,
   update_cwd = true,
