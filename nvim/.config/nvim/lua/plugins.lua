2d8c98abe904132392125ea1af0571b2d63bf7f4
Manuel <manutepowa@gmail.com>
2022-06-21 20:11:30 +0200

    Downgrade rest.nvim to commit 33c6829c8b612e92b731d8ff7c8b96a05afa1642

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -117,7 +117,8 @@ return require('packer').startup(function(use)
 	use "rcarriga/nvim-dap-ui"
 	use "Pocco81/DAPInstall.nvim"
 
-	use { "NTBBloodbath/rest.nvim", requires = { "nvim-lua/plenary.nvim" } }
+	use { "NTBBloodbath/rest.nvim", requires = { "nvim-lua/plenary.nvim" },
+		commit = "e5f68db73276c4d4d255f75a77bbe6eff7a476ef" }
 
 	-- Markdown
 	use { "ellisonleao/glow.nvim" }
