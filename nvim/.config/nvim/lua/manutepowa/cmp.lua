360345923761d720085af9bfbb27ad0f64acf326
Manuel <manutepowa@gmail.com>
2022-08-17 16:58:48 +0200

    Update cmp sources sorting

--- nvim/.config/nvim/lua/manutepowa/cmp.lua
+++ nvim/.config/nvim/lua/manutepowa/cmp.lua
@@ -29,6 +29,7 @@ local icons = require "manutepowa.icons"
 local kind_icons = icons.kind
 -- -- local lspkind = require "lspkind"
 -- vim.api.nvim_set_hl(0, "CmpItemKindCopilot", { fg = "#6CC644" })
+local compare = require "cmp.config.compare"
 
 cmp.setup({
   snippet = {
@@ -55,17 +56,6 @@ cmp.setup({
       else
         fallback()
       end
-      -- if cmp.visible() then
-      -- 	cmp.select_next_item()
-      -- elseif luasnip.expandable() then
-      -- 	luasnip.expand()
-      -- elseif luasnip.expand_or_jumpable() then
-      -- 	luasnip.expand_or_jump()
-      -- elseif check_backspace() then
-      -- 	fallback()
-      -- else
-      -- 	fallback()
-      -- end
     end, {
       "i",
       "s",
@@ -77,17 +67,11 @@ cmp.setup({
     fields = { "kind", "abbr", "menu" },
     format = function(entry, vim_item)
       vim_item.kind = kind_icons[vim_item.kind]
-      if entry.source.name == "copilot" then
-        vim_item.kind = icons.git.Octoface
-        vim_item.kind_hl_group = "CmpItemKindCopilot"
-      end
-
-      -- NOTE: order matters
       vim_item.menu = ({
         nvim_lsp = "",
-        nvim_lua = "",
-        luasnip = "",
         buffer = "",
+        luasnip = "",
+        nvim_lua = "",
         path = "",
         dap = ""
       })[entry.source.name]
@@ -96,10 +80,27 @@ cmp.setup({
   },
   sources = {
     { name = "nvim_lsp" },
-    { name = "nvim_lua" },
-    { name = "luasnip" },
     { name = "buffer" },
+    { name = "luasnip" },
+    { name = "nvim_lua" },
     { name = "path" }
+  },
+  sorting = {
+    priority_weight = 2,
+    comparators = {
+      compare.offset,
+      compare.exact,
+      -- compare.scopes,
+      compare.score,
+      compare.recently_used,
+      compare.locality,
+      -- compare.kind,
+      compare.sort_text,
+      compare.length,
+      compare.order,
+      -- require("copilot_cmp.comparators").prioritize,
+      -- require("copilot_cmp.comparators").score,
+    },
   },
   confirm_opts = {
     behavior = cmp.ConfirmBehavior.Replace,
