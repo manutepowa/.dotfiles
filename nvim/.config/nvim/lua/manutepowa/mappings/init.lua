128a26a00028e651645019ee586f4b5a1973c7ad
Manuel <manutepowa@gmail.com>
2022-08-30 23:48:34 +0200

    Change mappings with new vim api keymap

--- nvim/.config/nvim/lua/manutepowa/mappings/init.lua
+++ nvim/.config/nvim/lua/manutepowa/mappings/init.lua
@@ -15,71 +15,69 @@ map('', '<down>', '<nop>', { noremap = true })
 map('', '<left>', '<nop>', { noremap = true })
 map('', '<right>', '<nop>', { noremap = true })
 
-local nmap = map('n')
-local vmap = map('v')
-local imap = map('i')
-local xmap = map('x')
-local _map = map('')
+--[[ local nmap = map('n') ]]
 
 vim.g.mapleader = ' '
 
 -- Basic vim
-nmap('<A-j>', '5j', { noremap = true })
-nmap('<A-k>', '5k', { noremap = true })
-xmap('<A-j>', '5j', { noremap = true })
-xmap('<A-k>', '5k', { noremap = true })
-_map('<esc>', ':noh<CR>')
-nmap('<Leader>w', ':w<CR>')
-nmap('<Leader>q', ':q<CR>')
-imap('kj', '<ESC>')
-imap('<A-BS>', '<c-w>', { noremap = true })
+vim.keymap.set("n", '<A-j>', '5j', { noremap = true })
+vim.keymap.set("n", '<A-k>', '5k', { noremap = true })
+vim.keymap.set("x", '<A-j>', '5j', { noremap = true })
+vim.keymap.set("x", '<A-k>', '5k', { noremap = true })
+vim.keymap.set("", '<esc>', ':noh<CR>')
+vim.keymap.set("n", '<Leader>w', ':w<CR>')
+vim.keymap.set("n", '<Leader>q', ':q<CR>')
+vim.keymap.set("i", 'kj', '<ESC>')
+vim.keymap.set("i", '<A-BS>', '<c-w>', { noremap = true })
 
 -- Copy and paste without clipboard
-nmap('d', '"_d', { noremap = true })
-xmap('d', '"_d', { noremap = true })
-xmap('p', '"_dP', { noremap = true })
+vim.keymap.set("n", 'd', '"_d', { noremap = true })
+vim.keymap.set("x", 'd', '"_d', { noremap = true })
+vim.keymap.set("x", 'p', '"_dP', { noremap = true })
 
 -- Apaño para que no vuelva al inicio (autoindent)
-imap('<CR>', '<CR>x<BS>')
-nmap('o', 'ox<BS>')
-nmap('O', 'Ox<BS>')
+vim.keymap.set("i", '<CR>', '<CR>x<BS>')
+vim.keymap.set("n", 'o', 'ox<BS>')
+vim.keymap.set("n", 'O', 'Ox<BS>')
 
 -- Not select after line character
-vmap('$', '$h')
-nmap('0', '^')
+vim.keymap.set("v", '$', '$h')
+vim.keymap.set("n", '0', '^')
 
 -- Move right when open autopairs
-imap('<A-l>', '<Right>')
-imap('<A-h>', '<Left>')
+vim.keymap.set("i", '<A-l>', '<Right>')
+vim.keymap.set("i", '<A-h>', '<Left>')
 
 -- NVIM TREE
-nmap('<A-e>', ':NvimTreeToggle<CR>')
+vim.keymap.set("n", '<A-e>', ':NvimTreeToggle<CR>')
 
 -- Tabs resize
-nmap('<A-Down>', ':resize -5<CR>', { noremap = true, silent = true })
-nmap('<A-Up>', ':resize +5<CR>', { noremap = true, silent = true })
-nmap('<A-Left>', ':vertical resize -5<CR>', { noremap = true, silent = true })
-nmap('<A-Right>', ':vertical resize +5<CR>', { noremap = true, silent = true })
+vim.keymap.set("n", '<A-Down>', ':resize -5<CR>', { noremap = true, silent = true })
+vim.keymap.set("n", '<A-Up>', ':resize +5<CR>', { noremap = true, silent = true })
+vim.keymap.set("n", '<A-Left>', ':vertical resize -5<CR>', { noremap = true, silent = true })
+vim.keymap.set("n", '<A-Right>', ':vertical resize +5<CR>', { noremap = true, silent = true })
 
 
 -- Move block
-nmap('<C-j>', ":MoveLine(1)<CR>", { noremap = true, silent = true })
-nmap('<C-k>', ":MoveLine(-1)<CR>", { noremap = true, silent = true })
-vmap('<C-j>', ":MoveBlock(1)<CR>", { noremap = true, silent = true })
-vmap('<C-k>', ":MoveBlock(-1)<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", '<C-j>', ":MoveLine(1)<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", '<C-k>', ":MoveLine(-1)<CR>", { noremap = true, silent = true })
+vim.keymap.set("v", '<C-j>', ":MoveBlock(1)<CR>", { noremap = true, silent = true })
+vim.keymap.set("v", '<C-k>', ":MoveBlock(-1)<CR>", { noremap = true, silent = true })
 
 -- BufferLine
-nmap("<A-l>", ":bnext<CR>", { noremap = true, silent = true })
-nmap("<A-h>", ":bprevious<CR>", { noremap = true, silent = true })
-nmap("<A-c>", "<cmd>bdelete!<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<A-l>", ":bnext<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<A-h>", ":bprevious<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<A-c>", "<cmd>bdelete!<CR>", { noremap = true, silent = true })
 
 -- DiffGit
-nmap("<leader>dd", ":DiffviewOpen<CR>", { noremap = true, silent = true })
-nmap("<leader>df", ":DiffviewFileHistory %<CR>", { noremap = true, silent = true })
-nmap("gq", ":tabclose<CR>", { noremap = true })
+vim.keymap.set("n", "<leader>dd", ":DiffviewOpen<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>df", ":DiffviewFileHistory %<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "gq", ":tabclose<CR>", { noremap = true })
 
 -- Markdown viewer
-nmap("<leader>m", ":Glow<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>m", ":Glow<CR>", { noremap = true, silent = true })
 
 -- Codi inspector | Same that RunJS
--- nmap("<leader>c", ":Codi!!<CR>", { noremap = true, silent = true })
+-- vim.keymap.set("n","<leader>c", ":Codi!!<CR>", { noremap = true, silent = true })
+vim.keymap.set('c', '<A-j>', '<DOWN>', { noremap = true, silent = true })
+vim.keymap.set('c', '<A-k>', '<UP>', { noremap = true, silent = true })
