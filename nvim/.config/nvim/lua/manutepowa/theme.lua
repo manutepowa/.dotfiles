798bc62e3411f05ecbc38b650b197d5e99a15b03
Manuel <manutepowa@gmail.com>
2022-09-23 23:25:30 +0200

    New lualine style

--- nvim/.config/nvim/lua/manutepowa/theme.lua
+++ nvim/.config/nvim/lua/manutepowa/theme.lua
@@ -1,29 +1,18 @@
--- vim.g.onedark_style = 'warm'
--- vim.g.onedark_toggle_style_keymap = '<A-t>'
--- vim.g.onedark_italic_comment = true
--- vim.g.onedark_transparent_background = true
--- vim.g.onedark_darker_diagnostics = true
--- vim.g.onedark_hide_ending_tildes = true
--- commnet
-require('onedark').setup({
-  style = 'warm', -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
-  transparent = false, -- Show/hide background
-  term_colors = true, -- Use terminal colors
-  ending_tildes = false, -- Show the end-of-buffer tildes. By default they are hidden
-  toggle_style_key = '<leader>ts', -- Default keybinding to toggle
-  toggle_style_list = { 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer', 'light' },
-  code_style = {
-    comments = 'italic',
-    keywords = 'none',
-    functions = 'none',
-    strings = 'none',
-    variables = 'none'
-  },
-  -- Plugins Config --
-  diagnostics = {
-    darker = false, -- darker colors for diagnostic
-    undercurl = true, -- use undercurl instead of underline for diagnostics
-    background = false, -- use background color for virtual text
-  },
-})
-require('onedark').load()
+-- local colorscheme = "darkplus"
+local colorscheme = "darkplus"
+
+vim.g.onedarker_italic_keywords = false
+
+vim.g.onedarker_italic_functions = false
+
+vim.g.onedarker_italic_comments = true
+
+vim.g.onedarker_italic_loops = false
+
+vim.g.onedarker_italic_conditionals = false
+
+local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+if not status_ok then
+  -- vim.notify("colorscheme " .. colorscheme .. " not found!")
+  return
+end
