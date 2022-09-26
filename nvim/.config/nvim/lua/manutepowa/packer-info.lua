b9c55ef0f39d7fb7c24ce50413dbbfbce2bdea23
Manuel <manutepowa@gmail.com>
2022-09-26 10:36:31 +0200

    Update package-info config

--- nvim/.config/nvim/lua/manutepowa/packer-info.lua
+++ nvim/.config/nvim/lua/manutepowa/packer-info.lua
@@ -1,25 +1,25 @@
-require 'package-info'.setup{
-    colors = {
-        up_to_date = "#3C4048",
-        outdated = "#FFF",
-        -- outdated = "#d19a66",
+require 'package-info'.setup {
+  colors = {
+    up_to_date = "#3C4048",
+    outdated = "#d19a66",
+    -- outdated = "#d19a66",
+  },
+  icons = {
+    enable = true,
+    style = {
+      up_to_date = "|   ",
+      outdated = "|   ",
     },
-    icons = {
-        enable = true,
-        style = {
-            up_to_date = "|   ",
-            outdated = "|   ",
-        },
-    },
-    autostart = true,
-    hide_up_to_date = false,
-    hide_unstable_versions = false,
+  },
+  autostart = true,
+  hide_up_to_date = true,
+  hide_unstable_versions = false,
 }
 
 -- Show package versions
-vim.api.nvim_set_keymap(
-    "n",
-    "<leader>ns",
-    "<cmd>lua require('package-info').show({ force = true })<cr>",
-    { silent = true, noremap = true }
+vim.keymap.set(
+  "n",
+  "<leader>ns",
+  "<cmd>lua require('package-info').show({ force = true })<cr>",
+  { silent = true, noremap = true }
 )
