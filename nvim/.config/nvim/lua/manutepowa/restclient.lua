720fdc9f9b45248b4572e825a02e0fb38a1539e7
Manuel <manutepowa@gmail.com>
2022-12-02 09:39:59 +0100

    Add vnd to restclient

--- nvim/.config/nvim/lua/manutepowa/restclient.lua
+++ nvim/.config/nvim/lua/manutepowa/restclient.lua
@@ -1,24 +1,30 @@
 require("rest-nvim").setup({
-	-- Open request results in a horizontal split
-	result_split_horizontal = false,
-	-- Skip SSL verification, useful for unknown certificates
-	skip_ssl_verification = true,
-	-- Highlight request on run
-	highlight = {
-		enabled = true,
-		timeout = 150,
-	},
-	result = {
-		-- toggle showing URL, HTTP info, headers at top the of result window
-		show_url = true,
-		show_http_info = true,
-		show_headers = true,
-	},
-	-- Jump to request line on run
-	jump_to_request = false,
-	env_file = '.env',
-	custom_dynamic_variables = {},
-	yank_dry_run = true,
+  -- Open request results in a horizontal split
+  result_split_horizontal = false,
+  -- Skip SSL verification, useful for unknown certificates
+  skip_ssl_verification = true,
+  -- Highlight request on run
+  highlight = {
+    enabled = true,
+    timeout = 150,
+  },
+  result = {
+    formatters = {
+      json = "jq",
+      vnd = "jq",
+      html = function(body)
+        return vim.fn.system({ "tidy", "-i", "-q", "-" }, body)
+      end,
+    },
+    show_url = true,
+    show_http_info = true,
+    show_headers = true,
+  },
+  -- Jump to request line on run
+  jump_to_request = false,
+  env_file = '.env',
+  custom_dynamic_variables = {},
+  yank_dry_run = true,
 })
 
 vim.api.nvim_set_keymap('n', '<leader>r', ':lua require("rest-nvim").run()<CR>', { noremap = true })
