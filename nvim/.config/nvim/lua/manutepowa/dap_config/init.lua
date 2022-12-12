d8a75e5549722f19d02d0613857c8868ffe7cb8a
Manuel <manutepowa@gmail.com>
2022-12-12 13:52:41 +0100

    ..

--- nvim/.config/nvim/lua/manutepowa/dap_config/init.lua
+++ nvim/.config/nvim/lua/manutepowa/dap_config/init.lua
@@ -1,29 +1,38 @@
 local dap = require "dap"
-local g = vim.g
 
--- Mappings
-local function map(mode, lhs, rhs, opts)
-  local options = { noremap = true }
-  if opts then options = vim.tbl_extend('force', options, opts) end
-  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
-end
-
-map('n', '<leader>db', ':lua require"dap".toggle_breakpoint()<CR>')
-map('n', '<leader>dcb', ':lua require"dap".clear_breakpoints()<CR>')
-map('n', '<leader>dso', ':lua require"dap".step_out()<CR>')
-map('n', '<leader>dsi', ':lua require"dap".step_into()<CR>')
-map('n', '<A-CR>', ':lua require"dap".step_over()<CR>')
-map('n', '<leader>ds', ':lua require"dap".continue()<CR>')
-map('n', '<leader>drt', ':lua require"dap".repl.toggle()<CR>')
--- map('n', '<leader>dt', ':lua require"dap".terminate()<CR>')
-map('n', '<leader>dt', ':lua require("manutepowa.dap_config.dap-fn").stop()<cr>', { noremap = true })
-map('n', '<F2>', ':lua require("dapui").eval()<CR>')
-
-map('n', '<leader>dui', '<cmd>lua require"dapui".toggle()<CR>')
-map('n', '<leader>dhh', '<cmd>lua require"dapui.variables".hover()<CR>')
-map('v', '<leader>dhv', '<cmd>lua require"dapui.variables".visual_hover()<CR>')
 -- Enable DAP virtual text
-g.dap_virtual_text = true
+require("nvim-dap-virtual-text").setup {
+  enabled = true, -- enable this plugin (the default)
+  enabled_commands = false, -- create commands DapVirtualTextEnable, DapVirtualTextDisable, DapVirtualTextToggle, (DapVirtualTextForceRefresh for refreshing when debug adapter did not notify its termination)
+  highlight_changed_variables = false, -- highlight changed values with NvimDapVirtualTextChanged, else always NvimDapVirtualText
+  highlight_new_as_changed = false, -- highlight new variables in the same way as changed variables (if highlight_changed_variables)
+  show_stop_reason = true, -- show stop reason when stopped for exceptions
+  commented = false, -- prefix virtual text with comment string
+  only_first_definition = true, -- only show virtual text at first definition (if there are multiple)
+  all_references = false, -- show virtual text on all all references of the variable (not only definitions)
+  filter_references_pattern = '<module', -- filter references (not definitions) pattern when all_references is activated (Lua gmatch pattern, default filters out Python modules)
+  -- experimental features:
+  virt_text_pos = 'eol', -- position of virtual text, see `:h nvim_buf_set_extmark()`
+  all_frames = true, -- show virtual text for all stack frames not only current. Only works for debugpy on my machine.
+  virt_lines = false, -- show virtual lines instead of virtual text (will flicker!)
+  virt_text_win_col = nil -- position the virtual text at a fixed window column (starting from the first text column) ,
+  -- e.g. 80 to position at column 80, see `:h nvim_buf_set_extmark()`
+}
+
+vim.keymap.set('n', '<leader>db', ':lua require"dap".toggle_breakpoint()<CR>')
+vim.keymap.set('n', '<leader>dcb', ':lua require"dap".clear_breakpoints()<CR>')
+vim.keymap.set('n', '<leader>dso', ':lua require"dap".step_out()<CR>')
+vim.keymap.set('n', '<leader>dsi', ':lua require"dap".step_into()<CR>')
+vim.keymap.set('n', '<A-CR>', ':lua require"dap".step_over()<CR>')
+vim.keymap.set('n', '<leader>ds', ':lua require"dap".continue()<CR>')
+vim.keymap.set('n', '<leader>drt', ':lua require"dap".repl.toggle()<CR>')
+vim.keymap.set('n', '<leader>dt', ':lua require("manutepowa.dap_config.dap-fn").stop()<cr>', { noremap = true })
+vim.keymap.set('n', '<F2>', ':lua require("dapui").eval()<CR>')
+vim.keymap.set('n', '<leader>dui', '<cmd>lua require"dapui".toggle()<CR>')
+vim.keymap.set('n', '<leader>dhh', '<cmd>lua require"dapui.variables".hover()<CR>')
+vim.keymap.set('v', '<leader>dhv', '<cmd>lua require"dapui.variables".visual_hover()<CR>')
+
+
 
 dap.defaults.fallback.terminal_win_cmd = '80vsplit new'
 vim.fn.sign_define('DapBreakpoint', { text = '🟥', texthl = '', linehl = '', numhl = '' })
