113516fca4bcff56a6517d6fdc5f63be4c806c96
Manuel <manutepowa@gmail.com>
2023-03-09 14:16:27 +0100

    fix: Only manualy

--- nvim/.config/nvim/lua/manutepowa/projects.lua
+++ nvim/.config/nvim/lua/manutepowa/projects.lua
@@ -2,11 +2,7 @@ local project = require("project_nvim")
 
 project.setup({
   manual_mode = true,
-  -- detection_methods = { "lsp", "pattern" }, -- NOTE: lsp detection will get annoying with multiple langs in one project
-  detection_methods = { "pattern" },
-  -- patterns used to detect root dir, when **"pattern"** is in detection_methods
-  patterns = { ".git", "Makefile", "package.json" },
-  silent_chdir = false,
+  silent_chdir = true,
 })
 
 local telescope = require("telescope")
