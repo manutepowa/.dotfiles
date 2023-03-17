b2a03e13883393e2bea8d515b06649a291ce0e7d
Manuel <manutepowa@gmail.com>
2023-03-17 17:42:58 +0100

    ..

--- nvim/.config/nvim/lua/manutepowa/lsp/init.lua
+++ nvim/.config/nvim/lua/manutepowa/lsp/init.lua
@@ -6,4 +6,5 @@
 require "manutepowa.lsp.lsp_signature"
 require "manutepowa.lsp.mason"
 require("manutepowa.lsp.handlers").setup()
+require "manutepowa.lsp.saga"
 -- require "manutepowa.lsp.null-ls"
