1e644a2307f3cbd61e6ece545cdaec6f2ac91444
Manu <manutepowa@gmail.com>
2023-05-16 11:59:03 +0200

    feat: Add fonts to kitty

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,36 @@
+local wezterm = require("wezterm")
+
+local config = {
+  color_scheme = "Catppuccin Mocha",
+  enable_tab_bar = false,
+  window_decorations = "RESIZE",
+  font = wezterm.font("MonoLisaTrial", { weight = "Medium" }),
+  native_macos_fullscreen_mode = true,
+  keys = {
+    {
+      key = "n",
+      mods = "SHIFT|CTRL",
+      action = wezterm.action.ToggleFullScreen,
+    },
+  },
+  window_background_gradient = {
+    orientation = "Horizontal",
+    colors = {
+      -- "#0f0c29",
+      -- "#1e1e2d",
+      -- "#302b63",
+      "#00000C",
+      -- "#00003F",
+      "#000026",
+      "#00000C",
+      -- "#24243e",
+      -- "#1e1e2d",
+      -- "#0f0c29",
+    },
+    interpolation = "CatmullRom",
+    blend = "Rgb",
+    noise = 0,
+  },
+}
+
+return config
