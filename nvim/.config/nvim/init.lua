8ab122fa641e86289827e3f55f2f5d2c8451dcc7
Manu <manutepowa@gmail.com>
2023-06-12 18:11:30 +0200

    ..

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,5 +1,5 @@
 require 'config'
-require 'lazyp'
+require 'lazy-plugins'
 vim.api.nvim_create_autocmd("User", {
   group = vim.api.nvim_create_augroup("LazyVim", { clear = true }),
   pattern = "VeryLazy",
