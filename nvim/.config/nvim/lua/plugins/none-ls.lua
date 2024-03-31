e2d831e9f9948a93518f2ad93b2afbdab318d5af
Manu <manutepowa@gmail.com>
2024-03-31 14:31:00 +0200

    ..

--- nvim/.config/nvim/lua/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -3,18 +3,7 @@ return {
   opts = function(_, opts)
     local null_ls = require("null-ls")
     opts.sources = vim.list_extend(opts.sources or {}, {
-      null_ls.builtins.formatting.phpcsfixer,
-      -- nls.builtins.code_actions.gitsigns,
-      -- -- go
-      -- nls.builtins.code_actions.gomodifytags,
-      -- nls.builtins.code_actions.impl,
-      -- nls.builtins.diagnostics.golangci_lint,
-      -- -- ts
-      -- nls.builtins.formatting.biome,
-      -- require("typescript.extensions.null-ls.code-actions"),
-      -- other
-      -- nls.builtins.formatting.stylua,
-      -- nls.builtins.formatting.shfmt,
+      null_ls.builtins.formatting.pint,
     })
     return opts
   end,
