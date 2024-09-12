13ff6fd17649c5456e24e80ba31bd89593d181f2
Manu <manutepowa@gmail.com>
2024-09-12 23:59:47 +0200

    add avante ai

---
+++ nvim/.config/nvim/lua/plugins/avante.lua
@@ -0,0 +1,74 @@
+return {
+  {
+    "yetone/avante.nvim",
+    dependencies = {
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      --- The below dependencies are optional,
+      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+      "zbirenbaum/copilot.lua",      -- for providers='copilot'
+      {
+        -- support for image pasting
+        "HakonHarnes/img-clip.nvim",
+        event = "VeryLazy",
+        opts = {
+          -- recommended settings
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            -- required for Windows users
+            use_absolute_path = true,
+          },
+        },
+      },
+      {
+        -- Make sure to set this up properly if you have lazy=true
+        'MeanderingProgrammer/render-markdown.nvim',
+        opts = {
+          file_types = { "markdown", "Avante" },
+        },
+        ft = { "markdown", "Avante" },
+      },
+    },
+    event = "VeryLazy",
+    lazy = false,
+    build = "make",
+    opts = {
+      provider = "copilot",
+      -- copilot = {
+      --   endpoint = "https://api.anthropic.com",
+      --   model = "claude-3-5-sonnet-20240620",
+      --   temperature = 0,
+      --   max_tokens = 4096,
+      -- },
+      mappings = {
+        ask = "<leader>ai",
+        edit = "<leader>ae",
+        refresh = "<leader>ar",
+        diff = {
+          ours = "co",
+          theirs = "ct",
+          both = "cb",
+          next = "]x",
+          prev = "[x",
+        },
+        jump = {
+          next = "]]",
+          prev = "[[",
+        },
+        submit = {
+          normal = "<CR>",
+          insert = "<C-s>",
+        },
+        toggle = {
+          debug = "<leader>ad",
+          hint = "<leader>ah",
+        },
+      },
+    },
+  },
+}
