4af289b30245e10e0e2724462061737c1986a925
Manu <manutepowa@gmail.com>
2024-09-17 11:48:20 +0200

    rename auto window tab

--- nvim/.config/nvim/lua/plugins/avante.lua
+++ nvim/.config/nvim/lua/plugins/avante.lua
@@ -7,7 +7,6 @@ return {
       "MunifTanjim/nui.nvim",
       --- The below dependencies are optional,
       "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-      "zbirenbaum/copilot.lua",      -- for providers='copilot'
       {
         -- support for image pasting
         "HakonHarnes/img-clip.nvim",
@@ -25,26 +24,21 @@ return {
           },
         },
       },
-      {
-        -- Make sure to set this up properly if you have lazy=true
-        'MeanderingProgrammer/render-markdown.nvim',
-        opts = {
-          file_types = { "markdown", "Avante" },
-        },
-        ft = { "markdown", "Avante" },
-      },
+      -- {
+      --   -- Make sure to set this up properly if you have lazy=true
+      --   'MeanderingProgrammer/render-markdown.nvim',
+      --   opts = {
+      --     file_types = { "markdown", "norg", "rmd", "org", "Avante" },
+      --   },
+      --   ft = { "markdown", "norg", "rmd", "org", "Avante" },
+      -- },
     },
     event = "VeryLazy",
     lazy = false,
     build = "make",
     opts = {
-      provider = "copilot",
-      -- copilot = {
-      --   endpoint = "https://api.anthropic.com",
-      --   model = "claude-3-5-sonnet-20240620",
-      --   temperature = 0,
-      --   max_tokens = 4096,
-      -- },
+      provider = "gemini",
+      hints = { enabled = true },
       mappings = {
         ask = "<leader>ai",
         edit = "<leader>ae",
