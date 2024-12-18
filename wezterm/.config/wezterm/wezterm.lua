55a070d81579fe57fdcf6f96f5eec3f1f165dd21
Manuel <manutepowa@gmail.com>
2024-12-18 10:37:46 +0100

    Try to fix core.editor

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -3,19 +3,18 @@ local mux = wezterm.mux
 local act = wezterm.action
 local config = {}
 
--- =======================================================================
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
 -- =======================================================================
 config.disable_default_key_bindings = true
 config.keys = {
-  -- paste from the clipboard
-  { key = "p",     mods = "ALT",  action = act.PasteFrom("Clipboard") },
-  { key = "Enter", mods = "CTRL", action = act.ToggleFullScreen }, -- Desactivar ALT + Enter
+	-- paste from the clipboard
+	{ key = "p", mods = "ALT", action = act.PasteFrom("Clipboard") },
+	{ key = "Enter", mods = "CTRL", action = act.ToggleFullScreen }, -- Desactivar ALT + Enter
 }
 
 config.color_scheme = "OneHalfDark"
@@ -24,19 +23,19 @@ config.window_close_confirmation = "NeverPrompt"
 config.enable_tab_bar = false
 config.scrollback_lines = 2000
 config.window_padding = {
-  left = 0,
-  right = 0,
-  top = 0,
-  bottom = 0,
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
 }
 config.default_cursor_style = "SteadyBlock"
 config.colors = {
-  cursor_bg = "#CCC",       -- Color de fondo del cursor (rojo anaranjado)
-  cursor_fg = "#000",       -- Color de primer plano del cursor (blanco)
-  cursor_border = "#FF5733", -- Opcional: color del borde del cursor
+	cursor_bg = "#CCC", -- Color de fondo del cursor (rojo anaranjado)
+	cursor_fg = "#000", -- Color de primer plano del cursor (blanco)
+	cursor_border = "#FF5733", -- Opcional: color del borde del cursor
 
-  -- Esquema de colores personalizado
-  background = "#1C1C1C", -- Color de fondo del terminal
+	-- Esquema de colores personalizado
+	background = "#1C1C1C", -- Color de fondo del terminal
 }
 
 -- =======================================================================
@@ -54,18 +53,18 @@ config.enable_wayland = false
 config.initial_rows = 90
 config.initial_cols = 140
 wezterm.on("gui-startup", function(cmd) -- set startup Window position
-  local active = wezterm.gui.screens().active
+	local active = wezterm.gui.screens().active
 
-  -- Set the window coords on spawn.
-  local tab, pane, window = mux.spawn_window(cmd or {
-    x = active.x,
-    y = active.y,
-    width = active.width,
-    height = active.height,
-  })
+	-- Set the window coords on spawn.
+	local tab, pane, window = mux.spawn_window(cmd or {
+		x = active.x,
+		y = active.y,
+		width = active.width,
+		height = active.height,
+	})
 
-  -- You probably don't need both, but you can also set the positions after spawn.
-  window:gui_window():set_position(650, 0)
+	-- You probably don't need both, but you can also set the positions after spawn.
+	window:gui_window():set_position(650, 0)
 end)
 
 return config
