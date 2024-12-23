2612e8c57483494f93e7c2c2bd8ff9d014c4691c
Manuel <manutepowa@gmail.com>
2024-12-23 16:31:09 +0100

    improve indent-blankline

--- lazyvim/.config/lazyvim/lua/plugins/indentline.lua
+++ lazyvim/.config/lazyvim/lua/plugins/indentline.lua
@@ -2,10 +2,28 @@ return {
   "lukas-reineke/indent-blankline.nvim",
   main = "ibl",
   opts = {
-    indent = { char = "┊" },
+    indent = { char = "┊", tab_char = "┊" },
     whitespace = {
       remove_blankline_trail = true,
     },
-    scope = { enabled = false },
-  }
+    scope = { show_start = false, show_end = false },
+    exclude = {
+      filetypes = {
+        "Trouble",
+        "alpha",
+        "dashboard",
+        "help",
+        "lazy",
+        "mason",
+        "neo-tree",
+        "notify",
+        "snacks_dashboard",
+        "snacks_notif",
+        "snacks_terminal",
+        "snacks_win",
+        "toggleterm",
+        "trouble",
+      },
+    },
+  },
 }
