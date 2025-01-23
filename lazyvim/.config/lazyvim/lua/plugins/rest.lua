593e159a50489c98c5c0291f1563fbc0f1ec57cc
Manuel <manutepowa@gmail.com>
2025-01-23 11:48:53 +0100

    ..

--- lazyvim/.config/lazyvim/lua/plugins/rest.lua
+++ lazyvim/.config/lazyvim/lua/plugins/rest.lua
@@ -3,44 +3,48 @@ return {
   dependencies = {
     "MunifTanjim/nui.nvim",
     "nvim-lua/plenary.nvim",
-    "nvim-treesitter/nvim-treesitter"
+    "nvim-treesitter/nvim-treesitter",
   },
   ft = "hurl",
-  cmd = 'HurlRunnerAt',
+  cmd = "HurlRunnerAt",
   config = function(config)
-    local rest_nvim = require "hurl"
+    -- INSTALLATION: Install jq and hurl.
+    -- jq with homebrew: brew install jq
+    -- hurl with apt: Add ppa and install hurl
 
-    rest_nvim.setup {
+    local rest_nvim = require("hurl")
+
+    rest_nvim.setup({
       env_file = {
-        '.env',
+        ".env",
       },
       -- Show debugging info
-      debug = true,
+      debug = false,
       -- Show notification on run
       show_notification = false,
       -- Show response in popup or split
       mode = "split",
       -- Default formatter
       formatters = {
-        json = { 'jq' }, -- Make sure you have install jq in your system, e.g: brew install jq
+        json = { "jq" }, -- Make sure you have install jq in your system, e.g: brew install jq
         html = {
-          'prettier',    -- Make sure you have install prettier in your system, e.g: npm install -g prettier
-          '--parser',
-          'html',
+          "prettier", -- Make sure you have install prettier in your system, e.g: npm install -g prettier
+          "--parser",
+          "html",
         },
         xml = {
-          'tidy', -- Make sure you have installed tidy in your system, e.g: brew install tidy-html5
-          '-xml',
-          '-i',
-          '-q',
+          "tidy", -- Make sure you have installed tidy in your system, e.g: brew install tidy-html5
+          "-xml",
+          "-i",
+          "-q",
         },
       },
-    }
+    })
 
     vim.api.nvim_create_autocmd("FileType", {
       pattern = "hurl",
       callback = function()
-        local buff = tonumber(vim.fn.expand "<abuf>", 10)
+        local buff = tonumber(vim.fn.expand("<abuf>"), 10)
         vim.keymap.set(
           "n",
           "<leader>r",
