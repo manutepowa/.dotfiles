56e9abd1ec6344c9b7075aa462f95662566ef299
Manuel <manutepowa@gmail.com>
2025-01-27 09:03:26 +0100

    ..

--- lazyvim/.config/lazyvim/lua/plugins/avante.lua
+++ lazyvim/.config/lazyvim/lua/plugins/avante.lua
@@ -34,18 +34,18 @@ return {
       openai = {
         endpoint = "https://api.openai.com/v1",
         model = "gpt-4o-mini",
-        timeout = 15000,   -- Timeout reducido a 15 segundos
-        temperature = 0,   -- Ajuste de temperatura para más variabilidad
+        timeout = 15000, -- Timeout reducido a 15 segundos
+        temperature = 0, -- Ajuste de temperatura para más variabilidad
         max_tokens = 4096, -- Reducir el número máximo de tokens
         ["local"] = false,
       },
       gemini = {
-        endpoint = 'https://generativelanguage.googleapis.com/v1beta/models',
-        model = 'gemini-1.5-pro',
+        endpoint = "https://generativelanguage.googleapis.com/v1beta/models",
+        model = "gemini-1.5-pro",
         timeout = 30000, -- Timeout in milliseconds
         temperature = 0,
         max_tokens = 4096,
-        ['local'] = false,
+        ["local"] = false,
       },
       behaviour = {
         auto_suggestions = false, -- Experimental stage
@@ -57,8 +57,8 @@ return {
       hints = { enabled = true },
       windows = {
         position = "right", -- the position of the sidebar
-        wrap = true,        -- similar to vim.o.wrap
-        width = 55,         -- default % based on available width
+        wrap = true, -- similar to vim.o.wrap
+        width = 55, -- default % based on available width
         sidebar_header = {
           align = "center", -- left, center, right for title
           rounded = true,
