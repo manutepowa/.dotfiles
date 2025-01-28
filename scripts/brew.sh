bae2f77d3d8e46a9275ea0228434b2834682c491
Manuel <manutepowa@gmail.com>
2025-01-28 13:30:18 +0100

    ..

--- scripts/brew.sh
+++ scripts/brew.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
-# brew upgrade tmux eza fzf neovim bpytop translate-shell ripgrep
-brew upgrade
+# brew upgrade tmux eza fzf neovim bpytop translate-shell ripgrep fnm bat jq
 
-brew install tmux
-brew install eza
-brew install fzf
-brew install neovim
-brew install bpytop
-brew install translate-shell
-brew install ripgrep
-# Remove outdated versions from the cellar.
-brew cleanup
+#########
+
+# brew upgrade
+#
+# brew install tmux
+# brew install eza
+# brew install fzf
+# brew install neovim
+# brew install bpytop
+# brew install translate-shell
+# brew install ripgrep
+
+# brew cleanup
