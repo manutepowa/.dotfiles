32016e0568d9e148ff842d65f953c5e80d83a318
Manuel <manutepowa@gmail.com>
2025-03-07 23:36:21 +0100

    ..

--- lazyvim/.config/lazyvim/lua/config/keymaps.lua
+++ lazyvim/.config/lazyvim/lua/config/keymaps.lua
@@ -116,4 +116,3 @@ vim.keymap.set("n", "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft) -- Navigate to 
 vim.keymap.set("n", "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown) -- Navigate to the bottom pane
 vim.keymap.set("n", "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp) -- Navigate to the top pane
 vim.keymap.set("n", "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight) -- Navigate to the right pane
-vim.keymap.set("n", "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight) -- Navigate to the right pane
