037e9a0235c0a9f449a0135a60642599f404f380
Manuel <manutepowa@gmail.com>
2025-04-10 08:38:04 +0200

    ..

--- lazyvim/.config/lazyvim/lua/config/options.lua
+++ lazyvim/.config/lazyvim/lua/config/options.lua
@@ -1,59 +1,55 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
--- local options = {
---   backup = false, -- creates a backup file
---   clipboard = "unnamedplus", -- allows neovim to access the system clipboard
---   completeopt = { "menuone", "noselect" }, -- mostly just for cmp
---   conceallevel = 0, -- so that `` is visible in markdown files
---   -- diffopt = "filler,iwhite,internal,linematch:60,algorithm:patience",
---   fileencoding = "utf-8", -- the encoding written to a file
---   hlsearch = true, -- highlight all matches on previous search pattern
---   ignorecase = true, -- ignore case in search patterns
---   mouse = "", -- allow the mouse to be used in neovim
---   pumheight = 10, -- pop up menu height
---   showmode = false, -- we don't need to see things like -- INSERT -- anymore
---   smartcase = true, -- smart case
---   smartindent = true, -- make indenting smarter again
---   splitbelow = true, -- force all horizontal splits to go below current window
---   splitright = true, -- force all vertical splits to go to the right of current window
---   swapfile = false, -- creates a swapfile
---   termguicolors = true, -- set term gui colors (most terminals support this)
---   timeoutlen = 1000, -- time to wait for a mapped sequence to complete (in milliseconds)
---   undofile = true, -- enable persistent undo
---   updatetime = 100, -- faster completion (4000ms default)
---   writebackup = false, -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
---   expandtab = true, -- convert tabs to spaces
---   shiftwidth = 2, -- the number of spaces inserted for each indentation
---   tabstop = 2, -- insert 2 spaces for a tab
---   cursorline = true, -- highlight the current line
---   number = true, -- set numbered lines
---   laststatus = 3,
---   showcmd = false,
---   ruler = false,
---   relativenumber = false, -- set relative numbered lines
---   numberwidth = 2, -- set number column width to 2 {default 4}
---   signcolumn = "yes", -- always show the sign column, otherwise it would shift the text each time
---   wrap = false, -- display lines as one long line
---   scrolloff = 4,
---   sidescrolloff = 8,
---   title = true,
---   titleold = vim.split(os.getenv("SHELL") or "", "/")[3],
---   foldenable = true, -- enable fold for nvim-ufo
---   foldlevel = 99, -- set high foldlevel for nvim-ufo
---   foldlevelstart = 99, -- start with all code unfolded
---   statuscolumn = "",
--- }
+local options = {
+  -- backup = false, -- creates a backup file
+  -- clipboard = "unnamedplus", -- allows neovim to access the system clipboard
+  -- completeopt = { "menuone", "noselect" }, -- mostly just for cmp
+  -- conceallevel = 0, -- so that `` is visible in markdown files
+  -- diffopt = "filler,iwhite,internal,linematch:60,algorithm:patience",
+  -- fileencoding = "utf-8", -- the encoding written to a file
+  -- hlsearch = true, -- highlight all matches on previous search pattern
+  -- ignorecase = true, -- ignore case in search patterns
+  mouse = "", -- allow the mouse to be used in neovim
+  -- pumheight = 10, -- pop up menu height
+  -- showmode = false, -- we don't need to see things like -- INSERT -- anymore
+  -- smartcase = true, -- smart case
+  -- smartindent = true, -- make indenting smarter again
+  -- splitbelow = true, -- force all horizontal splits to go below current window
+  -- splitright = true, -- force all vertical splits to go to the right of current window
+  -- swapfile = false, -- creates a swapfile
+  -- termguicolors = true, -- set term gui colors (most terminals support this)
+  timeoutlen = 1000, -- time to wait for a mapped sequence to complete (in milliseconds)
+  -- undofile = true, -- enable persistent undo
+  -- updatetime = 100, -- faster completion (4000ms default)
+  -- writebackup = false, -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
+  -- expandtab = true, -- convert tabs to spaces
+  -- shiftwidth = 2, -- the number of spaces inserted for each indentation
+  -- tabstop = 2, -- insert 2 spaces for a tab
+  -- cursorline = true, -- highlight the current line
+  -- number = true, -- set numbered lines
+  -- laststatus = 3,
+  -- showcmd = false,
+  -- ruler = false,
+  relativenumber = false, -- set relative numbered lines
+  -- numberwidth = 2, -- set number column width to 2 {default 4}
+  signcolumn = "yes", -- always show the sign column, otherwise it would shift the text each time
+  -- wrap = false, -- display lines as one long line
+  -- scrolloff = 4,
+  -- sidescrolloff = 8,
+  -- title = true,
+  -- titleold = vim.split(os.getenv("SHELL") or "", "/")[3],
+  statuscolumn = "",
+}
 -- vim.opt.shortmess:append("c")
---
--- for k, v in pairs(options) do
---   vim.opt[k] = v
--- end
---
 
-vim.opt.relativenumber = false
-vim.opt.mouse = ""
-vim.opt.numberwidth = 1
+for k, v in pairs(options) do
+  vim.opt[k] = v
+end
+
+-- vim.opt.relativenumber = false
+-- vim.opt.mouse = ""
+-- vim.opt.numberwidth = 1
 
 local icons = require("config.icons")
 local signs = {
