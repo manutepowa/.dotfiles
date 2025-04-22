30a753c9d8018ed445327017a4e7c5b18290f0a5
Manuel <manutepowa@gmail.com>
2025-04-22 23:55:56 +0200

    snacks image

--- lazyvim/.config/lazyvim/README.md
+++ lazyvim/.config/lazyvim/README.md
@@ -2,3 +2,7 @@
 
 A starter template for [LazyVim](https://github.com/LazyVim/LazyVim).
 Refer to the [documentation](https://lazyvim.github.io/installation) to get started.
+
+![LazyVim Banner](https://cdn-icons-png.flaticon.com/512/25/25231.png)
+
+google
--- lazyvim/.config/lazyvim/lua/plugins/snacks.lua
+++ lazyvim/.config/lazyvim/lua/plugins/snacks.lua
@@ -20,7 +20,44 @@ return {
   opts = {
     bigfile = { enabled = true },
     statuscolumn = { enabled = false },
-    image = { enabled = true },
+    styles = {
+      snacks_image = {
+        relative = "editor",
+        col = -1,
+      },
+    },
+    image = {
+      enabled = true,
+      img_dirs = { "httpdocs/public" },
+      convert = {
+        notify = false, -- show a notification on error
+      },
+      doc = {
+        -- Personally I set this to false, I don't want to render all the
+        -- images in the file, only when I hover over them
+        -- render the image inline in the buffer
+        -- if your env doesn't support unicode placeholders, this will be disabled
+        -- takes precedence over `opts.float` on supported terminals
+        inline = false,
+        -- render the image in a floating window
+        -- only used if `opts.inline` is disabled
+        float = true,
+        -- Sets the size of the image
+        max_width = vim.g.neovim_mode == "skitty" and 20 or 40,
+        max_height = vim.g.neovim_mode == "skitty" and 10 or 10,
+        -- max_width = 60,
+        -- max_height = 30,
+        -- Apparently, all the images that you preview in neovim are converted
+        -- to .png and they're cached, original image remains the same, but
+        -- the preview you see is a png converted version of that image
+        --
+        -- Where are the cached images stored?
+        -- This path is found in the docs
+        -- :lua print(vim.fn.stdpath("cache") .. "/snacks/image")
+        -- For me returns `~/.cache/neobean/snacks/image`
+        -- Go 1 dir above and check `sudo du -sh ./* | sort -hr | head -n 5`
+      },
+    },
     words = { enabled = false },
     indent = {
       indent = {
