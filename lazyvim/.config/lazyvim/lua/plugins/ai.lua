dc9cdbed238eaf13b4c299a89ef92ce880c13f20
Manuel <manutepowa@gmail.com>
2025-05-07 14:09:15 +0200

    Add mcphub

--- lazyvim/.config/lazyvim/lua/plugins/ai.lua
+++ lazyvim/.config/lazyvim/lua/plugins/ai.lua
@@ -252,6 +252,16 @@ return {
           })
         end,
       },
+      extensions = {
+        mcphub = {
+          callback = "mcphub.extensions.codecompanion",
+          opts = {
+            make_vars = true,
+            make_slash_commands = true,
+            show_result_in_chat = true,
+          },
+        },
+      },
     },
     keys = {
       {
